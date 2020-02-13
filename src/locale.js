4b2aca9bc6a3ba3e708f72c8748c25955fbaa270
Andrey Saulin <Andrey.Saulin@aeroclub.ru>
2020-02-13 14:09:19 +0300

    ands

--- src/locale.js
+++ src/locale.js
@@ -23,6 +23,76 @@ const labels = {
         "ru" : "Эл.почта",
         "en" : "Mail"
     },
+    "title" : {
+        "ru" : "Заголовок",
+        "en" : "Title"
+    },
+    "startDate" : {
+        "ru" : "Дата начала",
+        "en" : "Start date"
+    },
+    "endDate" : {
+        "ru" : "Дата окончания",
+        "en" : "End date"
+    },
+    "date" : {
+        "ru" : "Дата",
+        "en" : "Date"
+    },
+    "time" : {
+        "ru" : "Время",
+        "en" : "Time"
+    },
+    "duration" : {
+        "ru" : "Продолжительность",
+        "en" : "Duration"
+    },
+    "country" : {
+        "ru" : "Страна",
+        "en" : "Сountry"
+    },
+    "region" : {
+        "ru" : "Регион",
+        "en" : "Region"
+    },
+    "city" : {
+        "ru" : "Город",
+        "en" : "City"
+    },
+    "street" : {
+        "ru" : "Улица",
+        "en" : "Street"
+    },
+    "address" : {
+        "ru" : "Адрес",
+        "en" : "Address"
+    },
+    "avatar" : {
+        "ru" : "Аватар",
+        "en" : "Avatar"
+    },
+    "image" : {
+        "ru" : "Картинка",
+        "en" : "Image"
+    },
+    "photoGallery" : {
+        "ru" : "Фото галерея",
+        "en" : "Photo gallery"
+    },
+    "description" : {
+        "ru" : "Описание",
+        "en" : "Description"
+    },
+    "firstName" : {
+        "ru" : "Имя",
+        "en" : "First name"
+    },
+    "secondName" : {
+        "ru" : "Фамилия",
+        "en" : "Second name"
+    },
+
+
     
 }
 
