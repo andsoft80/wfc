bb62a34a134d1bc9becb2f688ad05b5f3a45873c
Andrey Saulin <Andrey.Saulin@aeroclub.ru>
2020-02-20 18:42:25 +0300

    ands

--- src/components/PlaceCard.js
+++ src/components/PlaceCard.js
@@ -3,16 +3,29 @@ import labels from './PlaceCard_locale'
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import './form.css';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import InputLabel from '@material-ui/core/InputLabel';
+import { withStyles } from '@material-ui/core/styles';
 
 // function screen(width) {
 //   return (width < 700) ? 'sm' : 'lg';
 // }
+
+const styles = {
+  select: { padding: '10px' }
+};
 class PlaceCard extends React.Component {
   constructor(props) {
     super(props);
+    this.langs = [{ "_id": "ru", "name": "ru" }, { "_id": "en", "name": "en" }];
+    this.handleChange = this.handleChange.bind(this);
+    
 
     this.state = {
-
+      lang: "ru",
       locale: "ru",
       countries: [
         { "_id": "ru", "name": "Россия" },
@@ -29,41 +42,66 @@ class PlaceCard extends React.Component {
     };
 
   }
-
+  handleChange(e) {
+    this.setState({ lang: e.target.value });
+  }
 
   lab = (label) => {
     return labels[label][this.state.locale];
   }
 
   render() {
-
+    const { children, classes } = this.props;
 
     return (
 
       <div className='form-style-6'>
         <div className='flex'>
           <h1>{this.lab('newPlace')}</h1>
-          <input type="submit" value='Send' />
+
+
+
+          <Select
+            variant="outlined"
+            margin={'dense'}
+            className='flexrightSel'
+            id="demo-simple-select-outlined"
+            value={this.state.lang}
+            onChange={this.handleChange}
+          // labelWidth={labelWidth}
+          >
+
+            {this.langs.map((item) =>
+              <MenuItem key={item._id} value={item._id}>{item.name}</MenuItem>
+
+
+                )}
+                        
+          </Select>
+
+
+              <input type="submit" value='Send' className='flexright' />
         </div>
-        <form>
-          <Grid container spacing={2}>
-            {/* <Grid item xs={12} sm={6}>
+
+          <form>
+            <Grid container spacing={2}>
+              {/* <Grid item xs={12} sm={6}>
               <TextField id="firstName" label={this.lab('firstName')} variant="filled" name="firstName" helperText="Some important text" style={{ marginBottom: 0 }} fullWidth />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField id="secondName" label={this.lab('secondName')} variant="filled" name="secondName" helperText="Some important text" style={{ marginBottom: 0 }} fullWidth />
             </Grid> */}
-            <Grid item xs={12} >
-              <TextField id="title" label={this.lab('title')} variant="filled" name="title" style={{ marginBottom: 43 }} fullWidth type="text" />
+              <Grid item xs={12}>
+                <TextField id="title" label={this.lab('title')} variant="filled" name="title" style={{ marginBottom: 43, width:'50%' }} type="text" />
+              </Grid>
+              <Grid item xs={6} >
+                <input type="submit" value='Send' />
+              </Grid>
             </Grid>
-            <Grid item xs={6} >
-              <input type="submit" value='Send' />
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    );
-  }
-}
-
-export default PlaceCard;
+          </form>
+        </div>
+        );
+      }
+    }
+    
+    export default withStyles(styles)(PlaceCard);
